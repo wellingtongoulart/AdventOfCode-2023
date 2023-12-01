dfa151bf24f3a781c6df03fc7db65ee98af31962
wellingtongoulart <wellingtongoulart@outlook.com>
2023-12-01 20:12:55 -0300

    feat: raiz do projeto; feat: day 1.

---
+++ AdventOfCode/Day1/CalibrationValues.cs
@@ -0,0 +1,28 @@
+﻿namespace AdventOfCode.Day1;
+
+public class CalibrationValues
+{
+    private string PathFile = "C:\\Users\\Usuário\\Documents\\projetos\\AdventOfCode-2023\\AdventOfCode\\Day1\\exemplo.txt";
+    public string Conteudo {  get; set; }
+    public string[] Lista {  get; set; }
+
+    public CalibrationValues()
+    {
+        Conteudo = File.ReadAllText(PathFile);
+        Lista = File.ReadAllLines(PathFile);
+        EditarArquivoTxt();
+    }
+
+    public void EditarArquivoTxt()
+    {
+        if (Lista.Length > 1)
+        {
+            for (int i = 0; i < Lista.Length; i++) 
+            {
+                Lista[i] += ",";
+            }
+
+            File.WriteAllLines(PathFile, Lista);
+        }
+    }
+}
---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+
+using AdventOfCode.Day1;
+
+//List<string> fodas = new List<string>()
+//{
+//            "1abc2",
+//            "pqr3stu8vwx",
+//            "a1b2c3d4e5f",
+//            "treb7uchet"
+//};
+
+//Console.WriteLine(fodas.First());
+
+
+CalibrationValues fodas = new();
+
+
+
+
+
+
+
+
+
+
